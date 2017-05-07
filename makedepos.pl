1b5ff1be965fc90c93d1dd39d848cb614704a2ee
Peter Bräuer <pb866@york.ac.uk>
2017-05-07 10:39:39 +0100

    Allowing predefined values for vd
    Updating DSMACC auxiliary script makedepos.pl to read predefined 
    deposition velocities read in from input text file "depos.dat".
    Commenting source code, splitting into sections.
    1. section: Read in definitions from input file "depos.dat" with species
                names and predefined deposition velocities vd.
                Standard value in depos.dat can be assigned with key word
                'DEPOS' as species names. Columns are separated by white-
                spaces, comment lines start with '#'. If no standard vd is
                set vd = 5.0d-6 is used.
    2. section: Loop over all kpp files of current mechanism, find species
                definitions and retrieve species names. Assign standard vd
                unless a predefined value for vd exists.
    New sample input file 'depos.dat'.

--- makedepos.pl
+++ makedepos.pl
@@ -1,20 +1,93 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use List::MoreUtils qw(first_index); # to find first index of an entry in a list
 
-my $num= 0 ;
+### Variable declarations
+## Data arrays and variables
+my @dspc;     # species with predefined deposition velocities
+my @vd;       # deposition velocities
+my $mspc;     # species used in current mechanism
+my $vdstd;    # standard deposition velocitiy
+              # (defined with key word "DEPOS" in depos.dat)
+my $num= -1 ; # counter for reaction labels in output file
+
+## Temporary auxiliary arrays/variables
+#  @lines:  lines read in from input file
+#  @spl:    array with separated species and vd from input line
+#  $idx:    index of current species in species/vd array
+
+## file handling
+#  $dfil:      data file "depos.dat" with predefined deposition velocities
+#  $writefile: output KPP file "depos.kpp"
+#  $file:      input KPP files with definitions of species
+#              used in current mechanism
+
+########################################################################
+
+# Read in species and definitions of deposition velocities (vd)
+# from data file "depos.dat"
+open (my $dfil, '<', "depos.dat") or die "Could not open file $!";
+chomp(my @lines = <$dfil>);
+close($dfil);
+
+# Split array of input lines into array of species names and vd
+# unless it is an empty line or comment line starting with '#'
+foreach (@lines) {
+  if ($_ !~ /^\s*#/ && $_ !~ /^\s*$/) {
+    $_ =~ s/^\s+//;
+    print "$_\n";
+    my @spl = split(/\s+/, $_);
+    push @dspc, $spl[0];
+    push @vd, $spl[1];
+} }
+
+# Find standard value and save to vdstd.
+# If no standard value is defined in input file, use 5.00d-6.
+my $idx = first_index { $_ eq "DEPOS" } @dspc;
+if ($idx > 0) {
+  $vdstd = $vd[$idx];
+} else {
+  $vdstd = "5.00d-6"
+}
+
+print "vd(standard): ",$vdstd, "\n" ;
+
+########################################################################
+
+# Open output file and set KPP EQUATIONS variable
 open(my $writefile, ">","depos.kpp") or die "Could not open file  $!";
-#print $writefile "#DEFFIX\nDEPOS=IGNORE;\n";
 print $writefile "#EQUATIONS\n";
-for my $file ('inorganic','organic') {
-    open( FILE, "<$file.kpp" )  or die("Couldn't open :$!\\n");
 
-    while (<FILE>) {
-        if (/.*\=\s*IGNORE.*/) {
-            $num += 1            ;
-            s/\s*([\d\D]*)\s*\=\sIGNORE.*/{$num.} $1 = DUMMY : DEPOS*(1.16D-5*EXP(0.\/TEMP)) ;/g ;
-            print $writefile "$_" if $_ !~ /EMISS/;
-         }}
+# Loop over KPP files
+for my $file ('inorganic','organic') {
+  open( FILE, "<$file.kpp" )  or die("Couldn't open :$!\\n");
+# Find lines with species definitions
+  while (<FILE>) {
+    if (/.*\=\s*IGNORE.*/) {
+# Increase counter
+      $num += 1 ;
+# Get species and store in $mspc
+      s/\s*(\w)\s*\=\s*IGNORE.*/$1/g ;
+      $mspc = $_ ;
+      chomp $mspc ;
+
+# Define experimental values:
+      if (grep(/^$mspc$/, @dspc)) {
+        $idx = first_index { $_ eq $mspc } @dspc;
+        print $writefile
+        "\{$num\.\} $mspc = DUMMY :  DEPOS*\($vd[$idx]\) ;\n" ;
+# Otherwise use standard value:
+      } else {
+        $idx = first_index { $_ eq $mspc } @dspc;
+        print $writefile
+        "\{$num\.\} $mspc = DUMMY :  DEPOS*\($vdstd\) ;\n"
+        if $mspc !~ /EMISS/;
+  } } }
+
+# Close all files
     close(FILE);
 }
 close($writefile);
+
+########################################################################
