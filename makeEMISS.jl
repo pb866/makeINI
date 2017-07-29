26fb4fd14e7f9efd765952daddab809268cf97d3
Peter Bräuer <pb866@york.ac.uk>
2017-07-29 17:49:37 +0100

    unignore....

--- makeEMISS.jl
+++ makeEMISS.jl
@@ -4,4 +4,11 @@
 
 # Set standard values for missing file names from programme arguments
 push!(LOAD_PATH,"$(pwd())/jl.mod")
-using readfiles
+using DataFrames
+using readfiles, rddat
+# using test
+#
+# pstr(2.e-13,1.0)
+# println(fkpp,fdat,fout)
+emiss = rdini(fdat)
+println("\n\n$emiss")
